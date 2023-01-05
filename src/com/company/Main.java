abbafadc4826af8bfaf61b6a2b76de9e1225af0c
Alexander Alexeev <alekseev@trueconf.ru>
2023-01-05 23:43:08 +0100

    console output

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,11 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
-    }
+  public static void main(String[] args) {
+    int test = 10;
+    final int test1 = 20;
+    int res = test + test1;
+    String hello = "Hello world!";
+    System.out.printf("%s %d", hello, res);
+  }
 }
