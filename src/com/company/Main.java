04be97c9fa420a5139340e49041e5ca6c9577a28
Alexander Alexeev <alekseev@trueconf.ru>
2023-01-05 23:51:35 +0100

    console input

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
 
+    System.out.println("Input name...");
+    String name = scanner.nextLine();
+
+    System.out.println("Input age...");
+    int age = scanner.nextInt();
+
+    System.out.println("Input height");
+    float height = scanner.nextFloat();
+
+    System.out.printf("Name is %s;\nAge is %d;\nHeight is %f;", name, age, height);
   }
 }
